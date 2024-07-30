d72cc482b218e98497cee7fb092a36daf4b781b4
Giulio Riondino <riondino.giulio@gmail.com>
2024-07-29 19:20:50 -0700

    Add the drawing logic

--- scripts.js
+++ scripts.js
@@ -5,17 +5,26 @@ const container = document.querySelector(".container");
 const makeBox = () => {
     const height = `${Math.floor(parseFloat(getComputedStyle(container).height)) / numberOfSquares}px`;
     let box = document.createElement("div");
+
     box.classList.add("box-item");
-    box.style.height = height
-    box.style.width = height
+    box.style.height = height;
+    box.style.width = height;
+
+    box.addEventListener("mouseover", () => {
+        const list = box.classList;
+        if(list.contains("medium")) list.add("dark");
+        if(list.contains("light")) list.add("medium");
+        if (!list.contains("light")) list.add("light");
+        
+    })
 
     return box
 }
 
 const makeGrid = (squareArea) => {
     for (let i = 0; i <squareArea; i ++) {
-        container.appendChild(makeBox())
+        container.appendChild(makeBox());
     }
 }
 
-makeGrid(squareArea)
+makeGrid(squareArea);
